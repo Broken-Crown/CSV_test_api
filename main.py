64303c27b05ba271617e9f95ec2c30c59f5bd365
Affteron <Lontaredal@outlook.com>
2021-04-03 23:22:32 +0300

    UPDATE The dict value's type change to list. ADD Key and value sort.

--- main.py
+++ main.py
@@ -1,85 +1,44 @@
 import csv
 import time
-import json
+
+CSV_PATH = ".\\tmp\\recommends.csv"
 
 
 def time_counter(function):
-    def wrapper(search=None):
+    def wrapper():
         start_time = time.perf_counter()
-        function(search)
+        function()
         end_time = time.perf_counter()
         print(f"Время выполнения функции {function.__name__} составляет {end_time - start_time}")
-
     return wrapper
 
 
-# Линейный поиск O(n)
+# Сбор всей csv'ки в словарь. Ключом являются товары, а значением - множество строк вида "связанный_товар-rate".
 @time_counter
-def open_csv_without_list(searching_value=None):
-    # counter = 0
-    with open(".\\tmp\\recommends.csv", newline='') as csv_file:
-        reader = csv.reader(csv_file)
-        # for row in reader:
-        #     counter += 1
-        #     print(row)
-        #     if counter > 10:
-        #         break
-
-        if searching_value is not None:
-            [print(row) for row in reader if searching_value == row[0]]
-
-
-# В общем-то такая петрушка, но работа с raw-текстом
-@time_counter
-def open_csv_like_raw_text(searching_value=None):
-    with open(".\\tmp\\recommends.csv", 'r', encoding="utf_8_sig") as csv_file:
-        data = csv_file.readlines()
-        print(data[0])
-
-
-# Немного помучил yield
-def yield_test(searching_value=None):
-    with open(".\\tmp\\recommends.csv", "r", encoding="utf_8_sig") as csv_file:
-        # reader = csv.reader(csv_file)
-        # yield next(reader)
-        for row in csv_file.readline():
-            if row[0] == searching_value:
-                yield row
-
-
-# Аналогично
-def test_yield_search_time(searching_value=None):
-    for row in yield_test(searching_value):
-        print(row)
-
-
-# Сбор всей csv'ки в словарь. Ключём являются товары, а значением - множество строк вида "связанный_товар-rate".
-@time_counter
-def create_dick_from_csv(whats_search=None):
-    test_dict = {}
-    with open(".\\tmp\\recommends.csv", "r") as csv_file:
+def create_dict_from_csv():
+    large_dict = {}
+    with open(CSV_PATH, "r") as csv_file:
         reader = csv.reader(csv_file)
         for row in reader:
-            if row[0] in test_dict:
-                test_dict[row[0]].add(f"{row[1]}_{row[2]}")
-            elif row[0] not in test_dict:
-                test_dict[row[0]] = {f"{row[1]}_{row[2]}"}
-    print(len(test_dict.keys()))
-
-
-# Остаток от экспериментов с json (не надо так делать, но это не точно :D).
-@time_counter
-def search_in_json(whats_search):
-    with open(".\\tmp\\test_json.json", "r") as test_json:
-        jdata = json.load(test_json)
-        print(len(jdata.keys()))
+            if row[0] in large_dict:
+                large_dict[row[0]].append(f"{row[2]}_{row[1]}")
+            elif row[0] not in large_dict:
+                large_dict[row[0]] = [f"{row[2]}_{row[1]}"]
+    dict_keys_list = list(large_dict.keys())
+    dict_keys_list.sort()
+    for key, value in large_dict.items():
+        # large_dict[key] = list(dict.fromkeys(value))  # Это такой интересный способ удалить дубликаты.
+        value.sort()
+    print(dict_keys_list[0])
+    print(large_dict[dict_keys_list[0]])
+    print(len(dict_keys_list))
 
 
 # Обычный подсчёт элементов в csv O(n)
 @time_counter
-def csv_counter(whats_search=None):
+def csv_counter():
     counter = 0
-    with open(".\\tmp\\recommends.csv", "r") as csv_file:
+    with open(CSV_PATH, "r") as csv_file:
         for row in csv_file:
             counter += 1
     print(counter)
@@ -87,6 +46,5 @@ def csv_counter(whats_search=None):
 
 if __name__ == '__main__':
     print("-" * 150)
-    # csv_counter("1")
-    create_dick_from_csv()
+    create_dict_from_csv()
     print("-" * 150)
