8c7818bd745620cedb7d04687f6d38930380ffcd
Affteron <Lontaredal@outlook.com>
2021-04-05 16:17:38 +0300

    DEV Use OOP-paradigm for pre-treatment csv

--- main.py
+++ main.py
@@ -1,114 +1,113 @@
 import csv
 import os
+import sys
 import time
 
 
 def time_counter(function):
-    def wrapper(value, value_2):
+    def wrapper(value):
         start_time = time.perf_counter()
-        function(value, value_2)
+        function(value)
         end_time = time.perf_counter()
         print(f"Время выполнения функции {function.__name__} составляет {end_time - start_time}")
+
     return wrapper
-
-# Сбор всей csv'ки в словарь. Ключом являются товары, а значением - список строк вида "rate-связанный_товар".
-@time_counter
-def create_dict_from_csv(initial_csv_path, output_csv_path):
-    large_dict = {}
-
-    if os.path.isfile(initial_csv_path) is False:
-        raise FileExistsError("Initial *.csv-file not found")
-
-    with open(initial_csv_path, "r") as csv_file:
-        reader = csv.reader(csv_file)
-        for row in reader:
-            if row[0] in large_dict:
-                large_dict[row[0]].append(f"{row[2]} {row[1]}")
-            elif row[0] not in large_dict:
-                large_dict[row[0]] = [f"{row[2]} {row[1]}"]
-
-    dict_keys_list = list(large_dict.keys())
-    dict_keys_list.sort()
-
-    if os.path.isfile(output_csv_path) is False:
-        f = open(output_csv_path, "w")
-        f.close()
-
-    with open(output_csv_path, mode="w", newline='') as new_csv_file:
-        for key in dict_keys_list:
-            large_dict[key] = list(dict.fromkeys(large_dict[key]))
-            large_dict[key].sort()
-
-            csv_writer = csv.writer(new_csv_file, delimiter=',')
-
-            # Мне откровенно не нравится идея с временным списком, но другой пока что у меня нет
-            tmp_list = [key]
-
-            for value in large_dict[key]:
-                tmp_list.append(value.split()[1])
-                tmp_list.append(value.split()[0])
-
-            csv_writer.writerow(tmp_list)
 
 
 # Обычный подсчёт элементов в csv O(n)
-# @time_counter
-def csv_counter(file_path) -> None:
+def csv_counter(file_path):
     counter = 0
     with open(file_path, "r") as csv_file:
-        for row in csv_file:
+        for _ in csv_file:
             counter += 1
     print(counter)
 
 
-@time_counter
-def csv_separator(input_csv_path, output_csv_path) -> None:
-    if os.path.isdir(output_csv_path) is False:
-        os.mkdir(output_csv_path)
+class PreTreatmentCSV:
+    def __init__(self, initial_csv_file_path, sorted_csv_file_path, separated_csv_dir_path):
+        self.initial_csv_file_path = os.path.abspath(initial_csv_file_path)
+        self.sorted_csv_file_path = os.path.abspath(sorted_csv_file_path)
+        self.separated_csv_dir_path = os.path.abspath(separated_csv_dir_path)
+        self.__check_env()
 
-    with open(input_csv_path, "r", encoding="utf_8_sig") as csv_file:
-        first_line_in_csv = csv_file.readline()
+    def start_treatment(self):
+        self.__create_dict_from_csv()
+        self.__csv_separator()
 
-        # TODO: Да, я тут нарушил DRY потому что мысля пёрла.
-        new_file_name = first_line_in_csv[0].upper()
-        if "\\" in output_csv_path[-1]:
-            output_csv_path = output_csv_path[:-1]
-        new_file_path = f"{output_csv_path}\\{new_file_name}.csv"
+    def __check_env(self):
+        sorted_csv_dir_path = "\\".join(self.sorted_csv_file_path.split("\\")[0:-1])
 
-        # TODO: Заменить else на elif
-        if os.path.isfile(new_file_path) is False:
-            new_csv_file = open(new_file_path, "w", newline='')
-        elif os.path.isfile(new_file_path) is True:
-            new_csv_file = open(new_file_path, "a", newline='')
+        if os.path.isfile(self.initial_csv_file_path) is False:
+            raise FileExistsError("The initial file does not exist")
 
-        csv_writer = csv.writer(new_csv_file, delimiter=',')
+        if os.path.isdir(sorted_csv_dir_path) is False:
+            os.mkdir(sorted_csv_dir_path)
 
-        write_row = first_line_in_csv.split(",")
-        write_row[-1] = write_row[-1][:-1]
+        if os.path.isdir(self.separated_csv_dir_path) is False:
+            os.mkdir(self.separated_csv_dir_path)
 
-        csv_writer.writerow(write_row)
+    # Сбор всей csv'ки в словарь. Ключом являются товары, а значением - список строк вида "rate-связанный_товар".
+    @time_counter
+    def __create_dict_from_csv(self):
+        csv_large_dict = {}
 
-        for csv_row in csv_file.readlines():
-            if csv_row[0].upper() != new_file_name:
-                if new_csv_file.closed is False:
-                    new_csv_file.close()
+        with open(self.initial_csv_file_path, "r") as csv_file:
+            csv_reader = csv.reader(csv_file)
+            for row in csv_reader:
+                if row[0] in csv_large_dict:
+                    csv_large_dict[row[0]].append(f"{row[2]} {row[1]}")
+                elif row[0] not in csv_large_dict:
+                    csv_large_dict[row[0]] = [f"{row[2]} {row[1]}"]
 
-                new_file_name = csv_row[0].upper()
-                new_file_path = f"{output_csv_path}\\{new_file_name}.csv"
+        dict_keys_list = list(csv_large_dict.keys())
+        dict_keys_list.sort()
 
-                if os.path.isfile(new_file_path) is False:
-                    new_csv_file = open(new_file_path, "w", newline='')
-                else:
-                    new_csv_file = open(new_file_path, "a", newline='')
+        if os.path.isfile(self.sorted_csv_file_path) is False:
+            f = open(self.sorted_csv_file_path, "w")
+            f.close()
+
+        with open(self.sorted_csv_file_path, mode="w", newline='') as new_csv_file:
+            for key in dict_keys_list:
+                csv_large_dict[key] = list(dict.fromkeys(csv_large_dict[key]))
+                csv_large_dict[key].sort()
 
                 csv_writer = csv.writer(new_csv_file, delimiter=',')
 
-            write_row = csv_row.split(",")
-            write_row[-1] = write_row[-1][:-1]
+                # Мне откровенно не нравится идея с временным списком, но другой пока что у меня нет
+                tmp_list = [key]
 
-            csv_writer.writerow(write_row)
+                for value in csv_large_dict[key]:
+                    tmp_list.append(value.split()[1])
+                    tmp_list.append(value.split()[0])
 
-        new_csv_file.close()
+                csv_writer.writerow(tmp_list)
+
+    @time_counter
+    def __csv_separator(self):
+        with open(self.sorted_csv_file_path, "r", encoding="utf_8_sig") as csv_file:
+            new_file_name = ""
+            new_csv_file = ""
+            for csv_row in csv_file.readlines():
+                if csv_row[0].upper() != new_file_name:
+                    if new_csv_file and new_csv_file.closed is False:
+                        new_csv_file.close()
+
+                    new_file_name = csv_row[0].upper()
+                    new_file_path = f"{self.separated_csv_dir_path}\\{new_file_name}.csv"
+
+                    if os.path.isfile(new_file_path) is False:
+                        new_csv_file = open(new_file_path, "w", newline='')
+                    else:
+                        new_csv_file = open(new_file_path, "a", newline='')
+
+                    csv_writer = csv.writer(new_csv_file, delimiter=',')
+
+                write_row = csv_row.split(",")
+                write_row[-1] = write_row[-1][:-1]
+
+                csv_writer.writerow(write_row)
+
+            new_csv_file.close()
 
 
 # @time_counter
@@ -195,12 +194,6 @@ if __name__ == '__main__':
     sorted_csv_path = ".\\tmp\\new_recommends_1.csv"
     separated_csv_path = f".\\tmp\\separated csv\\"
 
-    create_dict_from_csv(init_csv_path, sorted_csv_path)
-    csv_separator(sorted_csv_path, separated_csv_path)
-    # csv_counter(NEW_CSV_PATH)
-    # find_some_row("plEWbqhEVb", 0.5)
-    # csv_separator()
-    # a = ["11", "22", "33", "44", "55", "66"]
-    # c = custom_bisect(a, 33.001)
-    # print(c)
+    PreTreatmentCSV(init_csv_path, sorted_csv_path, separated_csv_path).start_treatment()
+
     print("-" * 150)
